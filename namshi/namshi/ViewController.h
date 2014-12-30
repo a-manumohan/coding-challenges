ab65f49b0db9c616fac6b2be8e0b4cfb9d6728a6
manuMohan <manu@immedia.co.za>
2014-12-30 10:54:41 +0200

    Initial Commit
    project skeleton

---
+++ namshi/namshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  namshi
+//
+//  Created by Manu Mohan A on 2014/12/30.
+//  Copyright (c) 2014 namshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
